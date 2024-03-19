489e446d55b400f4996cc81c67bb11dbbe5ee5ee
Cristobal Andres Manzano Martinez <zano@live.cl>
2024-03-19 16:47:20 +0700

    update

--- src/server.js
+++ src/server.js
@@ -3,6 +3,14 @@ const { HTML_FILE_PATH, ROOMMATES_JSON_PATH, EXPENSES_JSON_PATH, ACTIVITIES_JSON
 const express = require('express');
 const fs = require('fs');
 
+// Configuración de CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 //* Import
 const { handleError } = require('./utils.js');
 const { addRoommate, addExpense, editExpense, deleteExpense } = require('./functions.js');
